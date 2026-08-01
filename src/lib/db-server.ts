081d12875d9b6077343db71fc71e7354b404240f
rabea0169 <rabeamahmoud0169@gmail.com>
2026-08-01 18:49:41 +0300

    revert: rollback db-server.ts to Prisma 6

--- src/lib/db-server.ts
+++ src/lib/db-server.ts
@@ -1,16 +1,11 @@
-import 'dotenv/config'
-import { PrismaClient } from '@/generated/prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as {
   prismaServer: PrismaClient | undefined
 }
 
-function createPrismaClient() {
-  const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
-  return new PrismaClient({ adapter, log: ['error'] })
-}
-
-export const db = globalForPrisma.prismaServer ?? createPrismaClient()
+export const db = globalForPrisma.prismaServer ?? new PrismaClient({
+  log: ['error'],
+})
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prismaServer = db
